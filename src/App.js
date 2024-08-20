4099071ca7e6d40097fff38448a29f3560f3447e
predrag jandric <predrag.jandric.bg@gmail.com>
2024-08-20 23:57:51 +0200

    logic in progress

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@ import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active, finished
+  status: "loading"
+} 
+
+function reducer(state, action){
+  switch (action.type) {
+    case: "dataReceived":
+      return {...state, 
+        questions: action.payload
+      }
+      
+  
+    default:
+     
+  }
+}
 
 function App() {
-const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({type: "dataReceived", payload: data}))
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <div className="app">
