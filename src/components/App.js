aacba2f61fb6fae0b5720331bcb32a2917beab0b
predrag jandric <predrag.jandric.bg@gmail.com>
2024-08-22 00:15:04 +0200

    if applied it will ADD: logic of displaying progress bar based on how many questions are answered correct or incorrect and points collected answering questions

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
-import NextButton from "./NextButton"
+import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -49,12 +50,17 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -75,6 +81,13 @@ function App() {
         )}
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               dispatch={dispatch}
               answer={answer}
